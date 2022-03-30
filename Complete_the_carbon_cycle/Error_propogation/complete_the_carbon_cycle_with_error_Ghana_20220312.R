a7b118356f1ca826724fefc4c822b645c46ef51c
Hzhang-ouce <hyzhang1996@gmail.com>
2022-03-30 14:09:40 +0100

    update NPP error propogation

--- Complete_the_carbon_cycle/Error_propogation/complete_the_carbon_cycle_with_error_Ghana_20220312.R
+++ Complete_the_carbon_cycle/Error_propogation/complete_the_carbon_cycle_with_error_Ghana_20220312.R
@@ -12,6 +12,8 @@ Sum_with_error <- function(x, na.rm=TRUE){
   sqrt(sum(x2))
 }
 
+# Sum_with_error is basically equation 1b in https://www.geol.lsu.edu/jlorenzo/geophysics/uncertainties/Uncertaintiespart2.html
+
 standard_error_calc <- function(x, na.rm=TRUE) {
   if (na.rm) x <- na.omit(x)
   sqrt(var(x)/length(x))
@@ -20,18 +22,19 @@ standard_error_calc <- function(x, na.rm=TRUE) {
 mutate_with_error = function(.data, f) {
   exprs = list(
     # expression to compute new variable values
-    deparse(f[[3]]),
+    deparse(f[[3]]) %>%
+      paste(collapse=''),
     # expression to compute new variable errors
     sapply(all.vars(f[[3]]), function(v) {
       dfdp = deparse(D(f[[3]], v))
-      sprintf('(d%s*(%s))^2', v, dfdp)
+      sprintf('(%s_se*(%s))^2', v, dfdp)
     }) %>%
       paste(collapse='+') %>%
       sprintf('sqrt(%s)', .)
   )
   names(exprs) = c(
     deparse(f[[2]]),
-    sprintf('d%s', deparse(f[[2]]))
+    sprintf('%s_se', deparse(f[[2]]))
   )
   myexprs <- purrr::map( exprs, rlang::parse_expr )
   
@@ -46,8 +49,8 @@ mutate_with_error = function(.data, f) {
 # How to use this? example here
 
 #  The idea is that, you have some variables (A, B, C) to mutate, you need to   
-#  call the standard error (or deviation) of them as dA, dB, dC, yes, just      
-#  add d, and then when you muate_with_error(new_thing = A + B *C), you will    
+#  call the standard error (or deviation) of them as A_se, B_se, C_se, yes, just      
+#  add _se, and then when you muate_with_error(new_thing = A + B *C), you will    
 #  auto get dnew_thing                                                          
 
 
@@ -55,7 +58,7 @@ mutate_with_error = function(.data, f) {
 #                                                                              .
 #  > data %>% mutate_with_error(z ~ eex*y)                                     .
 #                                                                              .
-#          eex          y        deex         dy           z         dz        .
+#          eex          y      eex_se       y_se           z       d_se        .
 #                                                                              .
 #  1 0.9238287 0.13630719 0.047671489 0.05553732 0.125924488 0.05171681        .
 #                                                                              .
@@ -75,7 +78,7 @@ mutate_with_error = function(.data, f) {
 
 # I just grabbed the values from Dr Sam Moore's paper, GCB 2018
 
-npp_table<-openxlsx::read.xlsx('trait_NPP_ghana_organized.xlsx',sheet='NPP_organised')
+npp_table<-openxlsx::read.xlsx('F:/Side_project/african_data_workshop/gem_code_examples/Complete_the_carbon_cycle/Error_propogation/trait_NPP_ghana_organized.xlsx',sheet='NPP_organised')
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -84,10 +87,15 @@ npp_table<-openxlsx::read.xlsx('trait_NPP_ghana_organized.xlsx',sheet='NPP_organ
 
 Stem_resp<-read_csv('F:/Oxford/Chapter_two/stem_respiration/output/stem_respiration_Ghana_20220310_HYZZ_per_plot.csv')%>%
   group_by(plot_code)%>%
-  summarise(MgC_year_per_ha=sum(MgC_year_per_ha,na.rm=T),
-            MgC_year_per_ha_se = Sum_with_error(MgC_year_per_ha_se,na.rm=T))%>%
+  summarise(MgC_year_per_ha=mean(MgC_year_per_ha,na.rm=T),
+            MgC_year_per_ha_se = Sum_with_error(MgC_year_per_ha_se,na.rm=T)/length(MgC_year_per_ha_se))%>%
   mutate(plot_code=str_replace_all(plot_code,'-','_'))
 
+
+#  # In the above line, I use Sum_with_error and divided by the number of       
+#  measurement to calculate final standard error as a mean over multiple        
+#  measurements (we believe that repeated measurement could reduce standard     
+#  error)                                                                       
 
 
 #...............................................................................
@@ -98,9 +106,36 @@ Stem_resp<-read_csv('F:/Oxford/Chapter_two/stem_respiration/output/stem_respirat
 
 combined<- npp_table %>%
             left_join(Stem_resp,by='plot_code')%>%
-  rename('Respiration_stem'=MgC_year_per_ha)%>%
-  rename('dRespiration_stem'=MgC_year_per_ha_se)%>%
-# note that now I have Respiration_stem and NPP, and dRespiration_stem and dNPP
-  mutate_with_error (NPP_plus_stem ~ Respiration_stem + NPP)
-# of course you can do XXXXX ~ Respiration_stem + NPP + Respiration_root 
-# and plus many more as long as you have corresponding dRespiration_root
+# note that now I have MgC_year_per_ha and NPP, and MgC_year_per_ha_se and NPP_se
+# you can customzie the below equation with anything, just make sure you have a corresponding _se
+  mutate_with_error (NPP_plus_stem ~ MgC_year_per_ha + NPP)
+# of course you can do XXXXX ~ MgC_year_per_ha + NPP + Respiration_root 
+# and plus many more as long as you have corresponding Respiration_root_se
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                              significance test                           ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# How to now whether the NPP_plus_stem is significantly different between plots? 
+
+# Z test hahahha, 
+
+# we are following this one https://cran.r-project.org/web/packages/distributions3/vignettes/two-sample-z-test.html
+
+ANK_01<- combined%>%
+  filter(plot_code == 'ANK_01')
+
+KOG_02<- combined%>%
+  filter(plot_code == 'KOG_02')
+
+simple_z_test<-function(mean1,mean2,se1,se2){
+  z_stat <- (mean1 - mean2) / 
+    sqrt(se1^2 + se2^2)
+  
+  p_value<-1 - pnorm(abs(z_stat)) + pnorm(-abs(z_stat))
+  
+  return(data.frame(z_stat,p_value))
+}
+
+anyway<-simple_z_test(ANK_01$NPP,KOG_02$NPP,ANK_01$NPP_se,KOG_02$NPP_se)
